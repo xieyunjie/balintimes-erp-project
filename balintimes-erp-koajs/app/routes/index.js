00406f12f2c95fbab6f9a8cf7bdd697653778be5
xieyunjie <yunjie.xie@hotmail.com>
2015-10-22 17:04:50 +0800

    返回object，可以加入koa-json对json进行优化。

--- balintimes-erp-koajs/app/routes/index.js
+++ balintimes-erp-koajs/app/routes/index.js
@@ -1,16 +1,24 @@
-
 var router = require("koa-router");
 var route = new router({
-  prefix:''
+    prefix: ''
 });
 
-route.get("/",function *(next){
-  yield  this.render('index', { title: 'Express' });
+route.get("/", function *(next) {
+    yield  this.render('index', {title: 'Express'});
 });
 
-route.get("/about",function *(next){
-  yield  this.render('about', { title: 'About',message:"这是一个about页面" });
+route.get("/about", function *(next) {
+    yield  this.render('about', {title: 'About', message: "这是一个about页面"});
 });
+
+route.get("/info", function *(next) {
+    var result = {
+        title: 'aaa',
+        name: "ss"
+    };
+    this.body = result;
+});
+
 
 module.exports = route;
 
