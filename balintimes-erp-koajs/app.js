39671135052870582eead8cfb98418d1e799e2bb
xieyunjie <yunjie.xie@hotmail.com>
2015-10-22 12:18:10 +0800

    加入路由功能，注意需要使用koa-router，不要使用koa-route

--- balintimes-erp-koajs/app.js
+++ balintimes-erp-koajs/app.js
@@ -1,60 +1,33 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var koa = require("koa"),
+    bodyParse = require("koa-body"),
+    render = require("koa-ejs"),
+    path = require('path'),
+    serve = require('koa-static'),
+    logger = require("koa-logger");
+var app = koa();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+app.use(logger());
+app.use(bodyParse());
+app.use(serve(path.join(__dirname, './static')));
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+render(app, {
+    root: path.join(__dirname, './app/views'),
+    layout: false,
+    viewExt: 'ejs',
+    cache: false,
+    debug: true
 });
 
-// error handlers
+var homeroute = require("./app/routes/index");
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
+app.use(homeroute.routes());
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+//app.use(route.all("/",homeroute));
+
+//app.use(route.get("/",indexView));
+//function *indexView(next){
+//    yield  this.render('index', { title: 'Express' });
+//}
 
 
 module.exports = app;
--- balintimes-erp-koajs/app/routes/index.js
+++ balintimes-erp-koajs/app/routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+//var route = require("koa-route");
+//
+//route.get("/",indexView);
+//
+//
+//function *indexView(next){
+//  yield  this.render('index', { title: 'Express' });
+//}
+//
+//module.exports = route;
+var router = require("koa-router");
+var route = new router({
+  prefix:''
 });
 
-module.exports = router;
+route.get("/",function *(next){
+  yield  this.render('index', { title: 'Express' });
+});
+
+route.get("/about",function *(next){
+  yield  this.render('about', { title: 'About',message:"这是一个about页面" });
+});
+
+module.exports = route;
+
