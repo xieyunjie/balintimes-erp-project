b0dac81222f03881dd7630a9d4cbcb5bce71651f
xieyunjie <yunjie.xie@hotmail.com>
2015-12-18 17:28:37 +0800

    写了一个基于koa-jwt认证的例子。

--- balintimes-erp-koajs/appJWT.js
+++ balintimes-erp-koajs/appJWT.js
@@ -1,12 +1,13 @@
 var koa = require("koa"),
-bodyParse = require("koa-body"),
-render = require("koa-ejs"),
-path = require('path'),
-serve = require('koa-static'),
-logger = require("koa-logger"),
-session = require('koa-generic-session');
+    bodyParse = require("koa-body"),
+    render = require("koa-ejs"),
+    path = require('path'),
+    serve = require('koa-static'),
+    logger = require("koa-logger"),
+    session = require('koa-generic-session');
 
 var router = require("koa-router");
+var jwt = require("koa-jwt");
 
 var app = koa();
 
@@ -17,7 +18,6 @@ app.use(bodyParse());
 app.use(serve(path.join(__dirname, './static')));
 
 
-
 render(app, {
     root: path.join(__dirname, './app/views'),
     layout: false,
@@ -26,17 +26,73 @@ render(app, {
     debug: true
 });
 
+var jwtSecret = "secret";
+
 var route = new router({
-    prefix: ''
+    prefix: '/jwt'
 });
 
-route.get("/jwt",function*(){
+route.get("/", function*() {
     yield this.render("jwt");
 });
+route.post("/sign", function *() {
+    var ctx = this;
+    var username = ctx.request.body.username, psw = ctx.request.body.password;
 
+    if (username != psw) {
+        this.body = "error";
+    }
+    else {
+        var result = {
+            name: username,
+            password: psw,
+            redisToken: "78&*45$%"
+        };
+        //jwt.sign(payload, secretOrPrivateKey, options, [callback])
+        //options --> //	algorithm (default: HS256)
+        //	expiresIn: expressed in seconds or an string describing a time span rauchg/ms. Eg: 60, "2 days", "10h", "7d"
+        //	audience
+        //	subject
+        //	issuer
+        //	noTimestamp
+        //	headers
+
+        var token = jwt.sign(result, jwtSecret);
+        result.token = token;
+        console.info(token);
+
+        this.body = result;
+    }
+
+});
 app.use(route.routes());
 
+// 以上的中间件不会被验证
 
+app.use(function *(next) {
+    try {
+        yield next;
+    } catch (err) {
+        if (401 == err.status) {
+            //this.status = 401;
+            this.body = '401 Unauthorized - Protected resource, use Authorization header to get access\n';
+        } else {
+            throw err;
+        }
+    }
+});
+// 验证是否合法？
+app.use(jwt({ secret: jwtSecret }));
+
+var route2 = new router({
+    prefix: '/jwt'
+});
+route2.get("/load", function *() {
+    var ctx = this;
+
+    this.body ="success";
+});
+app.use(route2.routes());
 
 
 app.use(function *pageNotFound(next) {
